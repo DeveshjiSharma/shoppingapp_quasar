446269fce47d9df063bdb4c028022b93a6c0b415
DeveshjiSharma <157876425+DeveshjiSharma@users.noreply.github.com>
2024-03-16 23:33:59 +0530

    Update HeroSection.jsx

--- src/components/herosection/HeroSection.jsx
+++ src/components/herosection/HeroSection.jsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
   return (
     <div>
       <img
-        src="https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg"
+        src="public\hero.jpg"
         alt=""
       />
     </div>
